35daa1fbaac3432f7bf62ef30653f69c63f9da63
vongoctam95 <vongoctamit@gmail.com>
2018-12-10 18:56:43 +0700

    upload news files

---
+++ main.js
@@ -0,0 +1,14 @@
+<scrip>
+function dropdownMenu() {
+    var x = document.getElementById('dropdownClick'),
+    if (x.className==="topnav") {
+        x-className+'reponsive'
+        // change topnav to topnav.reponsive
+        
+    } else {
+        x.className='topnav'
+        
+    }
+    
+}
+</scrip>
